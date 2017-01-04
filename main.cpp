0b51b0a00d5e450ead33265e4d369d569f516e12
Justin F <Jstnfaler@gmail.com>
2017-01-04 01:12:32 -0800

    v1.0.0.6

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 Justin Faler 
 1/1/2017 
 
-Version 1.0.0.4
+Version 1.0.0.6
 */
 #include <iostream>
 #include <string>
@@ -39,9 +39,10 @@ int main()
 	std::cout << "Ready to palletize? (y/n)" << endl;
 	cin >> y;
 
-	std::cout << "Press 1 to login " << endl;
-	std::cout << "Press 2 to verify " << endl;
-	std::cout << "press 3 for help" << endl;
+	std::cout << "Press 1 to login" << endl;
+	std::cout << "Press 2 to verify" << endl;
+	std::cout << "Press 3 to Print Packing Slip" << endl;
+	std::cout << "press 4 for help" << endl;
 	cin >> choice;
 
 
@@ -51,15 +52,16 @@ int main()
 
 	case 1: std::cout << "Please scan badge" << endl;
 		break;
-	case 2: std::cout << "2" << endl;
+	case 2: std::cout << "Verify pallet is full" << endl;
 		break;
-	case 3: std::cout << "3" << endl;
+	case 3: std::cout << "Printing label" << endl;
 		break;
-	case 4: std::cout << "4" << endl;
+	case 4: std::cout << "You must have permissions for this tool" << endl;
 		break;
-	case 5: std::cout << "5 help" << endl;
+	case 5: std::cout << "5" << endl;
 	}
 	cin >> choice; 
+
 
 	return 0;
 }
